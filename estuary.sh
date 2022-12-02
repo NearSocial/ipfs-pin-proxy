3bcbdcd9fa3871ab5cfa44ed426ba492a43033d2
Evgeny Kuzyakov <ek@nearprotocol.com>
2022-12-02 11:28:35 -0800

    bash

--- estuary.sh
+++ estuary.sh
@@ -3,6 +3,11 @@
 
 cd $(dirname "$0")
 mkdir -p estuary_logs
-DATE=$(date "+%Y_%m_%d")
 
-yarn estuary 2>&1 | tee -a estuary_logs/logs_$DATE.txt
+while :
+do
+  DATE=$(date "+%Y_%m_%d")
+  date | tee -a estuary_logs/logs_$DATE.txt
+  yarn estuary 2>&1 | tee -a estuary_logs/logs_$DATE.txt
+  sleep 5
+done
