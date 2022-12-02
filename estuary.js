ce1dae6ce634d2b612ce3fc6386423dfca520af9
Evgeny Kuzyakov <ek@nearprotocol.com>
2022-12-02 11:23:41 -0800

    Estuary uploads

---
+++ estuary.js
@@ -0,0 +1,95 @@
+require("dotenv").config();
+const fs = require("fs");
+const fsPromises = require("fs/promises");
+
+const FormData = require("form-data");
+const contentType = require("content-type");
+
+const axios = require("axios");
+
+const PostTimeout = 30000;
+
+function saveJson(json, filename) {
+  try {
+    const data = JSON.stringify(json);
+    fs.writeFileSync(filename, data);
+  } catch (e) {
+    console.error("Failed to save JSON:", filename, e);
+  }
+}
+
+function loadJson(filename, ignore) {
+  try {
+    let rawData = fs.readFileSync(filename);
+    return JSON.parse(rawData);
+  } catch (e) {
+    if (!ignore) {
+      console.error("Failed to load JSON:", filename, e);
+    }
+  }
+  return null;
+}
+
+(async () => {
+  const estuary = loadJson("estuary.json", true) || {
+    uploads: {},
+    success: {},
+  };
+  // const fn = `uploads-${Date.now()}.txt`;
+
+  // await fsPromises.rename("uploads.txt", fn);
+
+  const fn = "uploads.txt";
+  try {
+    const uploads = fs.readFileSync(fn, "utf8").trim().split("\n");
+    uploads.forEach((upload) => {
+      if (estuary.success[upload]) {
+        return;
+      }
+      estuary.uploads[upload] = true;
+    });
+  } catch {}
+
+  saveJson(estuary, "estuary.json");
+  try {
+    await fsPromises.rm(fn);
+  } catch {}
+
+  // console.log(estuary);
+  for (const upload of Object.keys(estuary.uploads)) {
+    try {
+      console.log("Fetching:", upload);
+      const res = await axios({
+        method: "get",
+        url: "https://ipfs.near.social/ipfs/" + upload,
+        timeout: PostTimeout,
+      });
+      const body = res.data;
+
+      const formData = new FormData();
+      formData.append("data", body, "img");
+
+      console.log(`Uploading ${body.length} bytes:`, upload);
+      await axios({
+        method: "post",
+        url: "https://upload.estuary.tech/content/add",
+        headers: {
+          Accept: "application/json",
+          Authorization: `Bearer ${process.env.ESTUARY_KEY}`,
+        },
+        data: formData,
+        timeout: PostTimeout,
+      });
+
+      estuary.success[upload] = true;
+      delete estuary.uploads[upload];
+      saveJson(estuary, "estuary.json");
+    } catch (e) {
+      console.error(e.toString());
+      break;
+    }
+  }
+
+  console.log("All done");
+  saveJson(estuary, "estuary.json");
+})();
